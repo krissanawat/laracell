17edb9fbd0beffc6d80dff582a29b35e9af2e0c0
krissanawat <krissanawat@flexmedia.co.th>
2013-09-05 16:56:52 +0700

    first initial

---
+++ src/Taqmaninw/Excel/Facades.php
@@ -0,0 +1,20 @@
+<?php namespace Taqmaninw\Excel;
+
+use Illuminate\Support\Facades\Facade;
+
+class Facades extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() {
+            return 'excel'; }
+
+}
+
+
+
+
+
